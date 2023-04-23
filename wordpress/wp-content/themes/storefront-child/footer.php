d875e13fa244a2853941c146cd4a79c60e3a4751
Sara Agnestrand <sara@agnestrand.net>
2023-04-23 15:05:12 +0200

    Plugin WP migrate

---
+++ wordpress/wp-content/themes/storefront-child/footer.php
@@ -0,0 +1,56 @@
+
+<!-- 
+// /**
+//  * Anpassad mall för footer.
+//  *
+//  * @package My Storefront Child Theme
+//  * @subpackage woocommerce/footer
+//  * @since 1.0.0
+//  */ -->
+
+<!-- Är detta rätt för en template override? Kan jag sedan bara lägga in en ändring 
+här???, 
+testade men fick då fel på min footer och den visades inte alls. Tror då att jag 
+ersattde det som står närmas under denna kommentar med det jag harkommernterat 
+ut ovanför. Testa de då att lägga in en echo... -->
+
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php do_action( 'storefront_before_footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+
+			<?php
+			/**
+			 * Functions hooked in to storefront_footer action
+			 *
+			 * @hooked storefront_footer_widgets - 10
+			 * @hooked storefront_credit         - 20
+			 */
+			do_action( 'storefront_footer' );
+			?>
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	<?php do_action( 'storefront_after_footer' ); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wordpress/wp-content/themes/storefront-child/functions.php
+++ wordpress/wp-content/themes/storefront-child/functions.php
@@ -1,1 +1,17 @@
 <!-- add action remove action -->
+<?php
+add_action( 'homepage', 'storefront_popular_products', 50 );
+function remove_storefront_popular_products (){
+    remove_action( 'homepage', 'storefront_popular_products', 50  );
+}
+
+add_action( 'init', 'remove_storefront_popular_products' );
+
+// add_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+
+// function add_text_after_shop_loop(){
+
+
+//     add_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+
+?>
